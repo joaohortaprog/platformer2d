54a80a3ad8b34e7fdd069f9113f7c58babee5e6f
joaohortaprog <joao.horta.prog@gmail.com>
2026-08-25 17:32:37 -0300

    implementing health item placeholder

---
+++ Assets/Scripts/Collectables/CollectableHealth.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableHealth : CollectableBase
+{
+    protected override void OnCollect()
+    {
+        base.OnCollect();
+        
+    }
+}
